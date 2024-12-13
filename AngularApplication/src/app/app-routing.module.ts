0336382f9cd3f142f8a33fd012dc848795a0a6d6
Ananya Shukla <annayashukla.ss290@gmail.com>
2024-12-13 18:50:22 +0530

    sign-up commit

--- AngularApplication/src/app/app-routing.module.ts
+++ AngularApplication/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },          // Default route
+    { path: 'seller', component: SellerAuthComponent  },  // Seller page
 
+//   { path: 'login', component: LoginComponent },    // Login page
+//   { path: 'cart', component: CartComponent },      // Cart page
+//   { path: '**', redirectTo: '', pathMatch: 'full' } // Wildcard route (handles unknown paths)
+ ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
